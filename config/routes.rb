ba8de81f0a029812b7dbdfe77b2e90a13aafbd3b
Aleks Gorbenko <kucher12@gmail.com>
2016-05-02 22:16:18 +0000

    added sign in/sign up/sing out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Nomster::Application.routes.draw do
-  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'places#index'
   resources :places
+
+  devise_for :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
