e1f39c8144bdedaf159687c39382bdacb74d3686
Aleks Gorbenko <kucher12@gmail.com>
2016-05-09 14:55:24 +0000

    added comment model, set relationships user-place-comment in db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+
   validates :name, presence: true, uniqueness: true, length: {within: 3..50 }
   validates :address, presence: true, length: {minimum: 10 }
   validates :description, presence: true, uniqueness: true, length: {minimum: 20 }
