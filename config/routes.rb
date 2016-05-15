7a59c12aef852ad7cd02a5fbade99f7046e3ad6f
Aleks Gorbenko <kucher12@gmail.com>
2016-05-15 14:26:04 +0000

    fixed the bug with the devise gem  sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Nomster::Application.routes.draw do
     resources :photos, only: :create
   end
 
-  resources :users, only: :show
-
   devise_for :users
+
+  resources :users, only: :show
 
 
   # Example of regular route:
